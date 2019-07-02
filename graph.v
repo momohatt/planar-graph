0d8f7e2b02b36709ab6487df0bbf60f3fa0b5de2
momohatt <momohatt10@gmail.com>
2019-07-02 16:02:36 +0900

    tmp

--- graph.v
+++ graph.v
@@ -33,13 +33,11 @@ Definition meet (r1 r2 : region) : Prop := nonempty (intersect r1 r2).
 (* relations (PERs).                                                      *)
 
 Record plain_map (m : map) : Prop := PlainMap {
-  (* if z2 belongs to the region containing z1,
-   * then either z1 belongs to the region containing z2 *)
   map_sym z1 z2 : m z1 z2 -> m z2 z1;
-  (* or the region of z2 is a subregion of that of z1 *)
   map_trans z1 z2 : m z1 z2 -> subregion (m z2) (m z1)
 }.
 
+(* sum of all the regions in map (set of all the points defined on map) *)
 Definition cover (m : map) : region := fun z => m z z.
 
 Definition submap (m1 m2 : map) : Prop := forall z, subregion (m1 z) (m2 z).
@@ -87,12 +85,78 @@ Definition not_corner (m : map) : region :=
 Definition adjacent (m : map) (z1 z2 : point) : Prop :=
   ~ m z1 z2 /\ meet (not_corner m) (border m z1 z2).
 
+Definition outerface (m : map) (z1 : point) : Prop :=
+  exists2 z2 : point, adjacent m z1 z2 & ~ (cover m z2).
+
+Record outerplane_map (m : map) : Prop := OuterplaneMap {
+  outerplane_map_finsimple : finite_simple_map m;
+  outerplane_map_outer z : outerface m z
+}.
+
 Record coloring (m k : map) : Prop := Coloring {
   coloring_plain : plain_map k;
-  coloring_cover : subregion (cover k) (cover m);
+  coloring_cover : subregion (cover m) (cover k);
   coloring_consistent : submap m k;
   coloring_adjacent z1 z2 : adjacent m z1 z2 -> ~ k z1 z2
 }.
 
+Definition totalize (m : map) : map :=
+  fun z z' =>
+    (exists z1 z2 : point,
+      closure (m z1) z /\ closure (m z2) z /\ adjacent m z1 z2 /\
+      intersect (border m z1 z2) (not_corner m) z') \/
+    (forall z1 z2 : point,
+      ~ (closure (m z1) z /\ closure (m z2) z /\ adjacent m z1 z2) /\
+      m z z').
+
+Lemma plain_map_totalize :
+  forall (m : map) (z z' : point),
+  simple_map m -> m z z' -> totalize m z z'.
+Proof.
+Admitted.
+
+Record tcoloring (m k : map) : Prop := TColoring {
+  tcoloring_coloring : coloring (totalize m) k;
+  tcoloring_adjacent z :
+    forall z1 z2 : point,
+    adjacent (totalize m) z z1 -> adjacent (totalize m) z z2 -> ~ k z1 z2
+}.
+
 Definition colorable_with (n : nat) (m : map) : Prop :=
   exists2 k, coloring m k & at_most_regions n k.
+
+Definition tcolorable_with (n : nat) (m : map) : Prop :=
+  exists2 k, tcoloring m k & at_most_regions n k.
+
+Lemma tcoloring_is_coloring :
+  forall m k : map, simple_map m -> tcoloring m k -> coloring m k.
+Proof.
+  intros.
+  destruct H.
+  destruct H0.
+  destruct tcoloring_coloring0.
+  split; auto.
+  - red. red. intros.
+    apply coloring_consistent0.
+    unfold totalize.
+    intros.
+
+    right; split; auto.
+    intros.
+    intros z1 z2 [[F1 F2] _].
+    unfold closure in F1.
+    inversion simple_map_plain0.
+    apply map_sym0 in H.
+    assert (meet (m z1) (m z0)). { apply F1; auto. }
+
+Admitted.
+
+Lemma coloring_leq_tcoloring :
+  forall m : map, forall n : nat,
+  tcolorable_with n m -> colorable_with n m.
+Proof with auto.
+  unfold colorable_with, tcolorable_with.
+  intros m n [k H0 H1].
+  exists k...
+  apply tcoloring_is_coloring...
+Qed.
