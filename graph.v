8f075f731eb2a2565f11d24d936fa389781b2add
momohatt <momohatt10@gmail.com>
2019-07-08 15:52:22 +0900

    tmp

--- graph.v
+++ graph.v
@@ -27,6 +27,17 @@ Definition nonempty (r : region) : Prop := exists z : point, r z.
 
 Definition subregion (r1 r2 : region) : Prop := forall z : point, r1 z -> r2 z.
 
+Lemma subregion_refl (r : region) :
+  subregion r r.
+Proof. red. auto. Qed.
+
+Lemma subregion_trans (r1 r2 r3 : region) :
+  subregion r1 r2 -> subregion r2 r3 -> subregion r1 r3.
+Proof.
+  unfold subregion. intros. auto. Qed.
+
+Hint Resolve subregion_refl subregion_trans.
+
 Definition meet (r1 r2 : region) : Prop := nonempty (intersect r1 r2).
 
 (* Maps are represented as relations; proper map are partial equivalence  *)
@@ -102,18 +113,24 @@ Record coloring (m k : map) : Prop := Coloring {
 
 Definition totalize (m : map) : map :=
   fun z z' =>
+    (* もともとm上の点である *)
+    m z z' \/
+    (* z1, z2を含むregionの間にある点 *)
     (exists z1 z2 : point,
       closure (m z1) z /\ closure (m z2) z /\ adjacent m z1 z2 /\
-      intersect (border m z1 z2) (not_corner m) z') \/
-    (forall z1 z2 : point,
-      ~ (closure (m z1) z /\ closure (m z2) z /\ adjacent m z1 z2) /\
-      m z z').
+      intersect (border m z1 z2) (not_corner m) z').
 
-Lemma plain_map_totalize :
-  forall (m : map) (z z' : point),
+Lemma plain_map_totalize (m : map) (z z' : point) :
   simple_map m -> m z z' -> totalize m z z'.
 Proof.
-Admitted.
+  intros. red. auto.
+Qed.
+
+Lemma totalize_subregion (m : map) :
+  simple_map m -> subregion (cover m) (cover (totalize m)).
+Proof.
+  unfold subregion, cover. intros. apply plain_map_totalize; auto.
+Qed.
 
 Record tcoloring (m k : map) : Prop := TColoring {
   tcoloring_coloring : coloring (totalize m) k;
@@ -132,31 +149,21 @@ Lemma tcoloring_is_coloring :
   forall m k : map, simple_map m -> tcoloring m k -> coloring m k.
 Proof.
   intros.
-  destruct H.
   destruct H0.
   destruct tcoloring_coloring0.
   split; auto.
-  - red. red. intros.
-    apply coloring_consistent0.
-    unfold totalize.
-    intros.
-
-    right; split; auto.
-    intros.
-    intros z1 z2 [[F1 F2] _].
-    unfold closure in F1.
-    inversion simple_map_plain0.
-    apply map_sym0 in H.
-    assert (meet (m z1) (m z0)). { apply F1; auto. }
-
+  - apply subregion_trans with (cover (totalize m)); auto.
+    apply totalize_subregion; auto.
+  - admit.
+  - admit.
 Admitted.
 
 Lemma coloring_leq_tcoloring :
   forall m : map, forall n : nat,
-  tcolorable_with n m -> colorable_with n m.
+  simple_map m -> tcolorable_with n m -> colorable_with n m.
 Proof with auto.
   unfold colorable_with, tcolorable_with.
-  intros m n [k H0 H1].
+  intros m n H [k H0 H1].
   exists k...
   apply tcoloring_is_coloring...
 Qed.
